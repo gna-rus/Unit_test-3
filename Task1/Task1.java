5f73cdffd0a248e600855efa72fcdb5a4e57eb55
gnarus-tm <71601039+gna-rus@users.noreply.github.com>
2023-09-21 12:25:09 +0300

    Create Task1.java

---
+++ Task1/Task1.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Task1 {
+    public boolean evenOddNumber(int n) {
+        if (n % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
