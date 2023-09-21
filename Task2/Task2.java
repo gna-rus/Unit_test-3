bd5077a0b5e9b9b2dc50532ca1ceb4e50b790210
gnarus-tm <71601039+gna-rus@users.noreply.github.com>
2023-09-21 12:27:14 +0300

    Create Task2.java

---
+++ Task2/Task2.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Task2 {
+    public boolean numberInInterval(int n) {
+        if ((n < 25) | (n > 100)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
